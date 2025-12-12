f3dec01b653ba51ce7472861037c8a14522e286d
Mike Pagel <mike@mpagel.de>
2025-12-12 20:29:41 +0100

    same as functions

--- platforms/pyscript-apps/src/apps/light_timer.py
+++ platforms/pyscript-apps/src/apps/light_timer.py
@@ -1,32 +1,28 @@
-import collections
+import asyncio
 
 from stubs.pyscript_builtins import log, service, task
 from stubs.pyscript_generated import light
 
-
-class LightTimer:
-    task = None
-
-    def run(self, entity_id: str, new_state: str):
-        # whenever state switched we will not continue with an existing timer task:
-        if self.task:
-            task.cancel(self.task)
-            self.task = None
-
-        if new_state == 'on':
-            self.task = task.create(LightTimer.turn_off_after, self, entity_id)
-
-    def turn_off_after(self, entity_id: str):
-        task.sleep(5)
-        log.info('Timer expired.')
-        light.turn_off(entity_id=entity_id)
-        self.task = None
+task_by_entity: dict[str, asyncio.Task] = {}
 
 
-light_timer_entity = collections.defaultdict(LightTimer)
+def turn_off_after(entity_id: str):
+    task.sleep(5)
+    log.info('Timer expired.')
+    light.turn_off(entity_id=entity_id)
+    del task_by_entity[entity_id]
+
+
+def run(entity_id: str, new_state: str):
+    # whenever state switched we will not continue with an existing timer task:
+    if entity_task := task_by_entity.get(entity_id):
+        task.cancel(entity_task)
+        del task_by_entity[entity_id]
+
+    if new_state == 'on':
+        task_by_entity[entity_id] = task.create(turn_off_after, entity_id)
 
 
 @service
 def light_timer(entity_id: str, new_state: str):
-    obj = light_timer_entity[entity_id]
-    LightTimer.run(obj, entity_id, new_state)
+    run(entity_id, new_state)
